894b33c90df2c44d4f11c9e70d59f0cce65b23a2
Anuj <206anuj@gmail.com>
2023-09-26 17:34:07 +0530

    registration and login

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from blogs.models import Category, Blog
+from .forms import RegistrationForm
+from django.contrib import auth
 
 def home(request):
     
@@ -13,3 +16,48 @@ def home(request):
         'posts' : posts,
     }
     return render(request, 'home.html', context=context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+
+    else:
+        form = RegistrationForm()
+    context = {
+    'form' : form,
+    }
+    return render(request, 'register.html', context=context)    
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+
+            return redirect('home')
+
+
+    form = AuthenticationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'login.html', context=context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+           
+                        
