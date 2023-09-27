b70ed4ea1ce925c02b41c17244fe4532c73097d6
anuj206 <y.206anuj@gmail.com>
2023-09-27 16:23:39 +0530

    category crud functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -47,7 +47,7 @@ def login(request):
             if user is not None:
                 auth.login(request, user)
 
-            return redirect('home')
+            return redirect('dashboard')
 
 
     form = AuthenticationForm()
